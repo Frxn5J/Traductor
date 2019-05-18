f4bfa6e6cf1feb5db40be0b7e30c291577ed36a1
Frxn5J <48078177+Frxn5J@users.noreply.github.com>
2019-05-17 23:31:36 -0500

    Create Index.php

---
+++ Index.php
@@ -0,0 +1,24 @@
+<html>
+    <head>
+        <title>TRADUCTOR ESPAÑOL A ESPAÑOL</title>
+        <style>
+            body{
+                color: cadetblue;
+            }
+            h1{
+                color: orange;
+                
+            }
+        </style>
+    </head>
+    <body>
+        <center>
+            <form action="" method="post">
+            <h1>TRADUCTOR ESPAÑOL ESPAÑOL</h1>
+            <textarea name="texto" cols="50" rows="30"></textarea><br><br>
+            <input type="submit" value="Traducir">
+            </form>
+        </center>
+    </body>
+    
+</html>
